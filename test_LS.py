b877b99ce4311fe6638a49d20ae6c4578ae9fa57
JerryChampionJr <91517008+JerryChampionJr@users.noreply.github.com>
2021-11-28 23:15:59 -0600

    Add files via upload

---
+++ test_LS.py
@@ -0,0 +1,16 @@
+#HW8 Test Champion
+
+import LS
+import pytest
+
+
+
+matrix1 = [[3,4,0],[-6,-8,1]]
+vector1 = [-1,7,2]
+matrix2 = [[3,4,7], [2,9,6], [1,5,3]]
+vector2 = [1,4,2]
+
+def test_least_squares():
+    assert LS.least_squares(matrix1,vector1) == [(1+0j), (1.9999999999999991+0j)]
+    assert LS.Least_Squares(matrix2, vector2) == [0.49999999999943157,-4.499999999994543,8.499999999989086]
+
